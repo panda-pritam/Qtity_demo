833287e4a73f87fe81e6c0e5555dcd8ed1206a57
panda-pritam <mondalrumki02@gmail.com>
2024-02-18 12:27:55 +0530

    test Commit

---
+++ qtify/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+// import './App.css';
+import NavBar from "./components/navBar/navBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ qtify/src/components/navBar/navBar.jsx
@@ -0,0 +1,13 @@
+import Logo from "../logo/logo";
+import Box from "@mui/system/Box";
+import SearchBox from "../searchBox/searchBar";
+import styles from "./NavBar.module.css";
+export default function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+      <Logo />
+      <SearchBox />
+      <button className={styles.feedback_button}>Give feedback</button>
+    </nav>
+  );
+}
